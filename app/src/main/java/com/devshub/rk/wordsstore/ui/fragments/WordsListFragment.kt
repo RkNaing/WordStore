c8ac42764c23b929ae78af82429a88b4556e9e35
Rahul Kumar <naing.rk09@gmail.com>
2018-12-30 10:22:42 +0630

    - Continue Implementation

--- app/src/main/java/com/devshub/rk/wordsstore/ui/fragments/WordsListFragment.kt
+++ app/src/main/java/com/devshub/rk/wordsstore/ui/fragments/WordsListFragment.kt
@@ -20,22 +20,34 @@ import kotlinx.android.synthetic.main.fragment_words.*
  **/
 class WordsListFragment : BaseFragment() {
 
+    companion object {
+        private const val ALL_WORDS = "Words"
+    }
+
     private val mainViewModel by lazy { ViewModelProviders.of(requireActivity()).get(MainViewModel::class.java) }
 
+    private var currentCategory = ALL_WORDS
+
+    private var shouldShowFilter = false
+
     override fun getLayoutId() = R.layout.fragment_words
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onViewReady(view: View, savedInstanceState: Bundle?) {
 
         wordsFragmentSwipeRefreshLayout.setOnRefreshListener {
             wordsFragmentSwipeRefreshLayout.isRefreshing = false
+            currentCategory = ALL_WORDS
             mainViewModel.getAllWords()
         }
 
         val adapter = WordsRVAdapter { wordWithCategory ->
             SaveWordDialogFragment.show(requireFragmentManager(), wordWithCategory)
         }
-
-        setHasOptionsMenu(true)
 
         wordsFragmentRvWords.adapter = adapter
         wordsFragmentRvWords.addItemDecoration(
@@ -52,14 +64,31 @@ class WordsListFragment : BaseFragment() {
             getAllWords()
 
             words.observe(this@WordsListFragment, Observer { it ->
+                val wordsCountLabel = "${it.count()} $currentCategory"
+                wordsFragmentTvCount.text = wordsCountLabel
                 adapter.submitList(it) {
                     if (adapter.itemCount > 0) {
                         wordsFragmentEmptyViewContainer.gone()
+                        wordsFragmentTvCount.visible()
                         wordsFragmentRvWords.visible()
+                        shouldShowFilter = true
                     } else {
+                        wordsFragmentTvEmptyMessage.text =
+                                if (currentCategory == ALL_WORDS) {
+                                    shouldShowFilter = false
+                                    getString(R.string.msg_empty_words)
+                                } else {
+                                    shouldShowFilter = true
+                                    getString(
+                                        R.string.msg_empty_words_by_category,
+                                        currentCategory
+                                    )
+                                }
                         wordsFragmentEmptyViewContainer.visible()
+                        wordsFragmentTvCount.gone()
                         wordsFragmentRvWords.gone()
                     }
+                    activity?.invalidateOptionsMenu()
                 }
             })
 
@@ -70,6 +99,8 @@ class WordsListFragment : BaseFragment() {
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.word_list_menu, menu)
+        val filterMenuItem = menu?.findItem(R.id.wordList_filter_categories)
+        filterMenuItem?.isVisible = shouldShowFilter
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -79,6 +110,7 @@ class WordsListFragment : BaseFragment() {
                 activity?.let { a ->
                     val categoriesListBottomSheet =
                         CategoryChooserBottomSheetDialogFragment.createInstance(getString(R.string.lbl_filter_by_category)) {
+                            currentCategory = it.title
                             mainViewModel.filterByCategoryId(it.id)
                         }
 
